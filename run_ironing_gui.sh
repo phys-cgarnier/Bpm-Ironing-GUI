894a8b55562d64d813d1c5b4499cfed563c10f3d
Chris Garnier <cgarnier@dev-srv09.slac.stanford.edu>
2025-06-18 09:34:06 -0700

    updated ironing gui to launch from physics_top

--- run_ironing_gui.sh
+++ run_ironing_gui.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+echo $SCRIPT_DIR
+IRON_SCRIPT="$SCRIPT_DIR/iron_bpms.py"
+echo $IRON_SCRIPT
 echo "Usage: $0 num_samples bsa_mode iron_mode"
 if [ "$#" -eq 3 ]; then
     echo "Using provided args"
-    pydm -m  "num=$1, mode=$2, iron_mode=$3" iron_bpms.py&
+    pydm -m  "num=$1, mode=$2, iron_mode=$3" $IRON_SCRIPT&
 else
-    pydm -m  "num=25, mode=Inclusion, iron_mode=0" iron_bpms.py 
-    
-fi
-
+    pydm -m  "num=50, mode=Inclusion, iron_mode=0" $IRON_SCRIPT&fi
